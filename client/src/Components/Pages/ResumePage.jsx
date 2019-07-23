7865e73ce0d2606cadb010ffe33da03e2c313483
Nicholas Domenichini <ndomenichini@LSI.CA>
2019-07-23 17:44:58 -0400

    Added header to resume

--- client/src/Components/Pages/ResumePage.jsx
+++ client/src/Components/Pages/ResumePage.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import { withStyles } from "@material-ui/core/styles";
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 const styles = theme => ({
-  root: {
-    padding: '24px'
+  centered: {
+    textAlign: 'center'
+  },
+  heading: {
+    color: '#5c6bc0'
+  },
+  address: {
+    fontStyle: 'italic'
+  },
+  contact: {
+    '& a': {
+      color: 'black',
+      textDecoration: 'none'
+    }
   }
 });
 
@@ -12,9 +26,19 @@ class ResumePage extends React.Component {
     const {classes} = this.props;
 
     return (
-      <div className={classes.root}>
-        <h1>Resume</h1>
-      </div>
+      <Container maxWidth="md">
+        <div className={classes.centered}>
+          <Typography variant="h3" className={classes.heading} gutterBottom>
+            Nicholas Domenichini
+          </Typography>
+          <Typography variant="subtitle1" className={classes.address}>
+            1077 Gordon Street, Unit 320, Guelph, Ontario, N1G 0E3
+          </Typography>
+          <Typography variant="subtitle1" className={classes.contact} gutterBottom>
+            <a href="tel:416-688-9726" >(416)-688-9726</a> | <a href="mailto:ndomenic@uoguelph.ca">ndomenic@uoguelph.ca</a> | <a href="https://domenichini.ca">domenichini.ca</a>
+          </Typography>
+        </div>
+      </Container>
     )
   }
 }
