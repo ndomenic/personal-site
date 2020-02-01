ad5df2ed5bd8fb97c6a6779833dbcf6f8a5cb919
Nick <ndomenic@uoguelph.ca>
2020-01-31 19:33:27 -0500

    Remove useless import

--- client/src/Components/Pages/AboutPage.jsx
+++ client/src/Components/Pages/AboutPage.jsx
@@ -6,7 +6,6 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
-import Alert from '@material-ui/lab/Alert';
 
 const aboutMeList = [
   {
